fbcdcb9d63c91a9a43e68c0935dcc7d075ee07f9
mahletseyoum101 <118665264+mahletseyoum101@users.noreply.github.com>
2025-01-12 14:55:13 +0300

    added docker file

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,13 @@
 
 from django.test import TestCase
 from .models import Order
+
+
+class OrderModelTest(TestCase):
+
+    def setUp(self):
+        Order.objects.create(item_id="123", quantity=2, customer_name="John Doe")
+
+    def test_order_creation(self):
+        order = Order.objects.get(item_id="123")
+        self.assertEqual(order.customer_name, "John Doe")
